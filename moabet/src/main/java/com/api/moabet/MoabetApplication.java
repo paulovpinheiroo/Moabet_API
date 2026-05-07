345442823ebfb37dc55b74ed64ed11804c61fe92
ADMITO-MITO <pauovictorpinheiro998663264@gmail.com>
2026-05-06 23:53:09 -0300

    Initial commit

---
+++ moabet/src/main/java/com/api/moabet/MoabetApplication.java
@@ -0,0 +1,13 @@
+package com.api.moabet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoabetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoabetApplication.class, args);
+	}
+
+}
---
+++ moabet/src/test/java/com/api/moabet/MoabetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.moabet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoabetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
