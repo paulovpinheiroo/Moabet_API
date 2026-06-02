0e49991532e60034fbce0fb1c5dfa530c5651c7d
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-06-02 16:27:32 -0300

    feat: add User entity with JPA and Lombok annotations

---
+++ moabet/src/main/java/com/api/moabet/models/User.java
@@ -0,0 +1,32 @@
+package com.api.moabet.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Column;
+
+@Entity(name = "User")
+@Table(name = "Users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+    @Column(name = "phone")
+    private String phone;
+}
