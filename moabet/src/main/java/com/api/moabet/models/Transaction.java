f4d6666e31471883e20406dd8c659170e094c155
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-06-02 22:12:30 -0300

    feat: add Transaction entity with Type enum and JPA annotations

---
+++ moabet/src/main/java/com/api/moabet/models/Transaction.java
@@ -0,0 +1,41 @@
+package com.api.moabet.models;
+
+import java.time.LocalDateTime;
+
+import com.api.moabet.models.enums.Type;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "Transaction")
+@Table(name = "Transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_transaction", nullable = false, unique = true)
+    private Long id;
+    @Column(name = "amount", nullable = false)
+    private Double amount;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private Type type;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    @ManyToOne
+    @JoinColumn(name = "id_wallet", nullable = false)
+    private Wallet wallet;
+}
