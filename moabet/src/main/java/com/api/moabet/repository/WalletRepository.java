7aa6329843ac535ab8406249dcecb40ccd4d17ca
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-06-05 20:11:16 -0300

    feat: implement findByUserId method

--- moabet/src/main/java/com/api/moabet/repository/WalletRepository.java
+++ moabet/src/main/java/com/api/moabet/repository/WalletRepository.java
@@ -1,9 +1,13 @@
 package com.api.moabet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.moabet.models.Wallet;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+
+    Optional<Wallet> findByUserId(Long userId);
 
 }
