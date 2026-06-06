3b4c0b923976484676818ab9c502070b8d956273
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-06-05 23:20:22 -0300

    fix: forgot post annotation

--- moabet/src/main/java/com/api/moabet/controller/BetController.java
+++ moabet/src/main/java/com/api/moabet/controller/BetController.java
@@ -1,5 +1,6 @@
 package com.api.moabet.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class BetController {
 
     private final BetService betService;
 
+    @PostMapping
     public BetResponseDTO createBet(@RequestBody BetRequestDTO betRequestDTO) {
         return betService.createBet(betRequestDTO);
     }
