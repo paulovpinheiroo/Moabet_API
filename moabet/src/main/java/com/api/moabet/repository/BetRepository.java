021cd8a7f8dd54b931afef07f0e8858f8c4ae038
ADMITO-MITO <pauovictorpinheiro998663264@gmail.com>
2026-06-22 15:47:30 -0300

    feat: add findByEventIdAndStatus query method to BetRepository

--- moabet/src/main/java/com/api/moabet/repository/BetRepository.java
+++ moabet/src/main/java/com/api/moabet/repository/BetRepository.java
@@ -1,9 +1,14 @@
 package com.api.moabet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.moabet.models.Bet;
+import com.api.moabet.models.enums.StatusBet;
 
 public interface BetRepository extends JpaRepository<Bet, Long> {
+
+    List<Bet> findByEventIdAndStatus(Long eventId, StatusBet status);
 
 }
