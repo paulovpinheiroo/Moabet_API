fc65a1af058a3a7433421ed36312ed766deb829d
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-06-28 17:51:12 -0300

    refactor: remove createUser from UserController in favor of AuthController

--- moabet/src/main/java/com/api/moabet/controller/UserController.java
+++ moabet/src/main/java/com/api/moabet/controller/UserController.java
@@ -5,12 +5,9 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.api.moabet.dto.users.UserRequestDTO;
 import com.api.moabet.dto.users.UserResponseDTO;
 import com.api.moabet.service.UserService;
 
@@ -21,11 +18,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    @PostMapping
-    public UserResponseDTO createUser(@RequestBody UserRequestDTO userRequestDTO) {
-        return userService.createUser(userRequestDTO);
-    }
 
     @GetMapping
     public List<UserResponseDTO> getAllUsers() {
