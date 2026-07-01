44ad9361507b379b2e97c5f91bb0d3feab80d7cc
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-07-01 02:30:49 -0300

    feat: add login endpoint with JWT token response in AuthController

--- moabet/src/main/java/com/api/moabet/controller/AuthController.java
+++ moabet/src/main/java/com/api/moabet/controller/AuthController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.api.moabet.dto.users.UserRequestDTO;
 import com.api.moabet.dto.users.UserResponseDTO;
+import com.api.moabet.dto.users.login.UserLoginRequestDTO;
+import com.api.moabet.dto.users.login.UserLoginResponseDTO;
+import com.api.moabet.service.AuthService;
 import com.api.moabet.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,10 +19,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final AuthService authService;
 
     @PostMapping("/register")
     public UserResponseDTO registerUser(@RequestBody UserRequestDTO userRequestDTO) {
         return userService.createUser(userRequestDTO);
     }
 
+    @PostMapping("/login")
+    public UserLoginResponseDTO loginUser(@RequestBody UserLoginRequestDTO userLoginRequestDTO) {
+        return authService.login(userLoginRequestDTO);
+    }
 }
