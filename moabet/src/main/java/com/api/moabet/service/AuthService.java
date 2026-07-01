bc28e82ce0e4dc67d43688c3fa05252fb1cad61f
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-07-01 02:40:36 -0300

    refactor: add password verification and token generation in AuthService

--- moabet/src/main/java/com/api/moabet/service/AuthService.java
+++ moabet/src/main/java/com/api/moabet/service/AuthService.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.api.moabet.dto.users.login.UserLoginRequestDTO;
+import com.api.moabet.dto.users.login.UserLoginResponseDTO;
 import com.api.moabet.models.User;
 import com.api.moabet.repository.UserRepository;
 
@@ -14,14 +15,15 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
 
-    public void login(UserLoginRequestDTO userLoginRequestDTO) {
+    public UserLoginResponseDTO login(UserLoginRequestDTO userLoginRequestDTO) {
         User user = userRepository.findByEmail(userLoginRequestDTO.email())
                 .orElseThrow(() -> new IllegalArgumentException("Email not found"));
         if (passwordEncoder.matches(userLoginRequestDTO.password(), user.getPassword())) {
-            // TODO: lança token JWT
+            return new UserLoginResponseDTO(tokenService.generateToken(user));
         } else {
-            // exceção
+            throw new IllegalArgumentException("Invalid password");
         }
     }
 
