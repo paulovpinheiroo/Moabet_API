65221458d7629d6fc9b84c747ceae023625c189f
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-06-30 22:48:49 -0300

    feat: add findByEmail query method to UserRepository

--- moabet/src/main/java/com/api/moabet/repository/UserRepository.java
+++ moabet/src/main/java/com/api/moabet/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.api.moabet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.moabet.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 
 }
