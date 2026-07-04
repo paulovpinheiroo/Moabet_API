c75d2f0274b6063a59f6ae025c042b3b24919059
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-07-03 23:54:03 -0300

    feat: enable method security for role-based authorization

--- moabet/src/main/java/com/api/moabet/config/SecurityConfig.java
+++ moabet/src/main/java/com/api/moabet/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.api.moabet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
